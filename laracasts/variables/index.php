3245eb09801ad4a964c455688607d71663600481
Andrew <andrew.j.heinke@gmail.com>
2017-01-28 11:28:38 -0600

    variables lesson

---
+++ laracasts/variables/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      header {
+        background-color: #efefef;
+        text-align: center;
+        padding: 2em;
+      }
+      h1 {
+
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1><?php echo "Hello, " . $_GET['name']; ?></h1>
+    </header>
+
+  </body>
+</html>
